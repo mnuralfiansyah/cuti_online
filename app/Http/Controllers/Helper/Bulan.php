df67da2445b758c7138adbec0437c2ca4a1470bf
M Nur Alfiansyah <mnuralfiansyah@gmail.com>
2018-08-17 01:04:05 +0800

    bismillah

--- app/Http/Controllers/Helper/Bulan.php
+++ app/Http/Controllers/Helper/Bulan.php
@@ -16,12 +16,12 @@
         'Desember'
       );
       $split = explode('-', $tanggal);
-      return $split[2] . ' ' . $bulan[ (int)$split[1] ] . ' ' . $split[0];
+      return $split[2].' '.$bulan[(int)$split[1]].' '.$split[0];
     }
 
     function date_indo($tanggal)
     {
       $bulan = array (1 =>1,2,3,4,5,6,7,8,9,10,11,12);
       $split = explode('-', $tanggal);
-      return $split[2] .'-'. $bulan[ (int)$split[1] ] .'-'. $split[0];
+      return $split[2].'-'.$bulan[(int)$split[1]].'-'.$split[0];
     }
